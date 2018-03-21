2e79161548479e3c7d797e01544217f3b5c70060
Sheila <sheilasaia@users.noreply.github.com>
2018-03-21 11:50:33 -0400

    finished malachite calibration of all pools

--- malachite_calibration_script.R
+++ malachite_calibration_script.R
@@ -4,6 +4,10 @@
 
 # ---- 1 load libraries ----
 
+# clear ws
+rm(list = ls())
+
+# load packages
 library(tidyverse)
 
 # ---- 2 load data ----
@@ -15,6 +19,7 @@ setwd("/Users/ssaia/Documents/phd/oxygen_isotopes/bonn_soil_analysis/data/malach
 nahco3_data_raw = read_csv("raw_malachite_nahco3.csv", col_names = FALSE)
 naoh_data_raw = read_csv("raw_malachite_naoh.csv", col_names = FALSE)
 hcl_data_raw = read_csv("raw_malachite_hcl.csv", col_names = FALSE)
+ox_data_raw = read_csv("raw_malachite_ox.csv", col_names = FALSE)
 
 # set working directory for soil extraction weights
 setwd("/Users/ssaia/Documents/phd/oxygen_isotopes/bonn_soil_analysis/data/site_and_exp_design_data/")
@@ -79,7 +84,6 @@ for (i in 1:num_samples) {
 vol_soln_added = 30 #volume of solution added for extraction (ml)
 nahco3_dilution = 200/20 #dilution multiple i.e. 10x dilution is represented as 10
 phosphate_molec_wt=95 #95 g of phosphate = 1 mol
-#nahco3 dulted 200/20 = 10x, naoh diluted 200/5 = 40x, hcl diluted 200/5 = 40x, ox diluted 200/1 = 200x
 nahco3_data = nahco3_samples %>%
   mutate(rawP_ppm = abs * nahco3_slope + nahco3_intercept) %>%
   left_join(soil_wts_sel, by = "AssayID") %>% # join soil_wts_data
@@ -99,5 +103,206 @@ write_csv(nahco3_data, "malachite_nahco3_calibrated.csv")
 
 # ---- 4 naoh data reformatting and processing ----
 
+# standards
+naoh_standards = data.frame(std_abs = t(naoh_data_raw[1,1:8]), # standard absorbances from plate reader data
+                              vol_added_ul = c(0,20,40,60,80,100,120,200),
+                              P_ppm=c(0,0.1,0.2,0.3,0.4,0.5,0.6,1))
+
+# fit standards
+naoh_lm = lm(P_ppm ~ std_abs, data = naoh_standards)
+
+# print summary to check
+summary(naoh_lm)
+
+# save variables for plotting
+naoh_intercept = summary(naoh_lm)$coefficients[1,1]
+naoh_slope = summary(naoh_lm)$coefficients[2,1]
+naoh_rsquared = summary(naoh_lm)$r.squared
+
+# plot line and observations for standard curve
+plot(P_ppm ~ std_abs, data = naoh_standards, pch = 16, xlab = "Absorbance", ylab = "Known PO4 as P Concentration (ppm)",
+     main = paste("y =", round(naoh_slope, 4), "x + ", round(naoh_intercept, 4), ", r-squared =", round(naoh_rsquared, 4), sep=" "))
+abline(a = naoh_intercept, b = naoh_slope, col = "red", lwd = 3)
+
+# make new dataframe to add sample abs readings
+samp_slots = 96-12 #96-12 because first row are standards
+naoh_samples = data.frame(AssayID = rep(0, samp_slots), abs = rep(0, samp_slots))
+
+# for loop to save abs data to sample table
+naoh_data_transposed = as.data.frame(t(naoh_data_raw)) #transpose dataframe if samples are across rows
+strt = 1 #start iterator outside loop to keep track of samples
+for (i in 2:8) {
+  end = strt+11
+  naoh_samples$abs[strt:end] = naoh_data_transposed[,i]
+  strt = strt + 12 #add 12 to iterator
+}
+
+# define AssayID for sample data
+num_samples = 30
+assay_id_list = seq(1:num_samples)
+num_reps = 3
+num_reps_per_sample = c(rep(c(3, 3, 2), num_samples/num_reps)) #number of reDPtitions you analyze each sample (A and B have 3 reps, C has 2 reps)
+strt = 1
+for (i in 1:num_samples) {
+  end = strt + num_reps_per_sample[i] - 1
+  naoh_samples$AssayID[strt:end] = rep(assay_id_list[i], num_reps_per_sample[i])
+  strt = strt + num_reps_per_sample[i]
+}
+
+# calculate raw and calibrated concentration
+vol_soln_added = 30 #volume of solution added for extraction (ml)
+naoh_dilution = 200/5 #dilution multiple i.e. 40x dilution is represented as 40
+phosphate_molec_wt=95 #95 g of phosphate = 1 mol
+naoh_data = naoh_samples %>%
+  mutate(rawP_ppm = abs * naoh_slope + naoh_intercept) %>%
+  left_join(soil_wts_sel, by = "AssayID") %>% # join soil_wts_data
+  mutate(row_num = seq(1:dim(naoh_samples)[1])) %>%
+  mutate(Extraction = "naoh", Method = "malachite", P_pool = "DP") %>%
+  mutate(P_mgperkg = rawP_ppm * (vol_soln_added / dry_soil_added_g) * naoh_dilution) %>% # inorganic P pool = DP for dissolved P
+  mutate(P_umol = (P_mgperkg * dry_soil_added_g) / phosphate_molec_wt) %>% # DP in micromoles
+  select(row_num, Bonn_SampleID, SampleID, Month, Rep, Extraction:P_umol) %>%
+  na.omit()
+
+# set working directory
+setwd("/Users/ssaia/Documents/phd/oxygen_isotopes/bonn_soil_analysis/data/malachite_data/")
+
+# export
+write_csv(naoh_data, "malachite_naoh_calibrated.csv")
 
 
+# ---- 5 hcl data reformatting and processing ----
+
+# standards
+hcl_standards = data.frame(std_abs = t(hcl_data_raw[1,1:8]), # standard absorbances from plate reader data
+                            vol_added_ul = c(0,20,40,60,80,100,120,200),
+                            P_ppm=c(0,0.1,0.2,0.3,0.4,0.5,0.6,1))
+
+# fit standards
+hcl_lm = lm(P_ppm ~ std_abs, data = hcl_standards)
+
+# print summary to check
+summary(hcl_lm)
+
+# save variables for plotting
+hcl_intercept = summary(hcl_lm)$coefficients[1,1]
+hcl_slope = summary(hcl_lm)$coefficients[2,1]
+hcl_rsquared = summary(hcl_lm)$r.squared
+
+# plot line and observations for standard curve
+plot(P_ppm ~ std_abs, data = hcl_standards, pch = 16, xlab = "Absorbance", ylab = "Known PO4 as P Concentration (ppm)",
+     main = paste("y =", round(hcl_slope, 4), "x + ", round(hcl_intercept, 4), ", r-squared =", round(hcl_rsquared, 4), sep=" "))
+abline(a = hcl_intercept, b = hcl_slope, col = "red", lwd = 3)
+
+# make new dataframe to add sample abs readings
+samp_slots = 96-12 #96-12 because first row are standards
+hcl_samples = data.frame(AssayID = rep(0, samp_slots), abs = rep(0, samp_slots))
+
+# for loop to save abs data to sample table
+hcl_data_transposed = as.data.frame(t(hcl_data_raw)) #transpose dataframe if samples are across rows
+strt = 1 #start iterator outside loop to keep track of samples
+for (i in 2:8) {
+  end = strt+11
+  hcl_samples$abs[strt:end] = hcl_data_transposed[,i]
+  strt = strt + 12 #add 12 to iterator
+}
+
+# define AssayID for sample data
+num_samples = 30
+assay_id_list = seq(1:num_samples)
+num_reps = 3
+num_reps_per_sample = c(rep(c(3, 3, 2), num_samples/num_reps)) #number of reDPtitions you analyze each sample (A and B have 3 reps, C has 2 reps)
+strt = 1
+for (i in 1:num_samples) {
+  end = strt + num_reps_per_sample[i] - 1
+  hcl_samples$AssayID[strt:end] = rep(assay_id_list[i], num_reps_per_sample[i])
+  strt = strt + num_reps_per_sample[i]
+}
+
+# calculate raw and calibrated concentration
+vol_soln_added = 30 #volume of solution added for extraction (ml)
+hcl_dilution = 200/5 #dilution multiple i.e. 40x dilution is represented as 40
+phosphate_molec_wt=95 #95 g of phosphate = 1 mol
+hcl_data = hcl_samples %>%
+  mutate(rawP_ppm = abs * hcl_slope + hcl_intercept) %>%
+  left_join(soil_wts_sel, by = "AssayID") %>% # join soil_wts_data
+  mutate(row_num = seq(1:dim(hcl_samples)[1])) %>%
+  mutate(Extraction = "hcl", Method = "malachite", P_pool = "DP") %>%
+  mutate(P_mgperkg = rawP_ppm * (vol_soln_added / dry_soil_added_g) * hcl_dilution) %>% # inorganic P pool = DP for dissolved P
+  mutate(P_umol = (P_mgperkg * dry_soil_added_g) / phosphate_molec_wt) %>% # DP in micromoles
+  select(row_num, Bonn_SampleID, SampleID, Month, Rep, Extraction:P_umol) %>%
+  na.omit()
+
+# set working directory
+setwd("/Users/ssaia/Documents/phd/oxygen_isotopes/bonn_soil_analysis/data/malachite_data/")
+
+# export
+write_csv(hcl_data, "malachite_hcl_calibrated.csv")
+
+
+# ---- 6 ox data reformatting and processing ----
+
+# standards
+ox_standards = data.frame(std_abs = t(ox_data_raw[1,1:8]), # standard absorbances from plate reader data
+                           vol_added_ul = c(0,20,40,60,80,100,120,200),
+                           P_ppm=c(0,0.1,0.2,0.3,0.4,0.5,0.6,1))
+
+# fit standards
+ox_lm = lm(P_ppm ~ std_abs, data = ox_standards)
+
+# print summary to check
+summary(ox_lm)
+
+# save variables for plotting
+ox_intercept = summary(ox_lm)$coefficients[1,1]
+ox_slope = summary(ox_lm)$coefficients[2,1]
+ox_rsquared = summary(ox_lm)$r.squared
+
+# plot line and observations for standard curve
+plot(P_ppm ~ std_abs, data = ox_standards, pch = 16, xlab = "Absorbance", ylab = "Known PO4 as P Concentration (ppm)",
+     main = paste("y =", round(ox_slope, 4), "x + ", round(ox_intercept, 4), ", r-squared =", round(ox_rsquared, 4), sep=" "))
+abline(a = ox_intercept, b = ox_slope, col = "red", lwd = 3)
+
+# make new dataframe to add sample abs readings
+samp_slots = 96-12 #96-12 because first row are standards
+ox_samples = data.frame(AssayID = rep(0, samp_slots), abs = rep(0, samp_slots))
+
+# for loop to save abs data to sample table
+ox_data_transposed = as.data.frame(t(ox_data_raw)) #transpose dataframe if samples are across rows
+strt = 1 #start iterator outside loop to keep track of samples
+for (i in 2:8) {
+  end = strt+11
+  ox_samples$abs[strt:end] = ox_data_transposed[,i]
+  strt = strt + 12 #add 12 to iterator
+}
+
+# define AssayID for sample data
+num_samples = 30
+assay_id_list = seq(1:num_samples)
+num_reps = 3
+num_reps_per_sample = c(rep(c(3, 3, 2), num_samples/num_reps)) #number of reDPtitions you analyze each sample (A and B have 3 reps, C has 2 reps)
+strt = 1
+for (i in 1:num_samples) {
+  end = strt + num_reps_per_sample[i] - 1
+  ox_samples$AssayID[strt:end] = rep(assay_id_list[i], num_reps_per_sample[i])
+  strt = strt + num_reps_per_sample[i]
+}
+
+# calculate raw and calibrated concentration
+vol_soln_added = 30 #volume of solution added for extraction (ml)
+ox_dilution = 200/1 #dilution multiple i.e. 200x dilution is represented as 200
+phosphate_molec_wt=95 #95 g of phosphate = 1 mol
+ox_data = ox_samples %>%
+  mutate(rawP_ppm = abs * ox_slope + ox_intercept) %>%
+  left_join(soil_wts_sel, by = "AssayID") %>% # join soil_wts_data
+  mutate(row_num = seq(1:dim(ox_samples)[1])) %>%
+  mutate(Extraction = "ox", Method = "malachite", P_pool = "DP") %>%
+  mutate(P_mgperkg = rawP_ppm * (vol_soln_added / dry_soil_added_g) * ox_dilution) %>% # inorganic P pool = DP for dissolved P
+  mutate(P_umol = (P_mgperkg * dry_soil_added_g) / phosphate_molec_wt) %>% # DP in micromoles
+  select(row_num, Bonn_SampleID, SampleID, Month, Rep, Extraction:P_umol) %>%
+  na.omit()
+
+# set working directory
+setwd("/Users/ssaia/Documents/phd/oxygen_isotopes/bonn_soil_analysis/data/malachite_data/")
+
+# export
+write_csv(ox_data, "malachite_ox_calibrated.csv")
